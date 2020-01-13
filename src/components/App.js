bab26659ff2785d2420d88a356f777a57342abad
David Huang <david.huang@cerence.com>
2020-01-13 17:11:50 -0500

    add stream client -- first commit

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+      <a href="/pagetwo">Naviagte to Page Two</a>
+    </div>
+  );
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo<button>Click Me!</button>
+      <a href="/">Naviagte to Page One</a>
+    </div>
+  );
+};
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" exact component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
