592fdf1614e22b8eb0347cb1d2f9b78287753de4
SolidFastidious <davidhuang6492@gmail.com>
2020-04-19 04:06:32 -0400

    commit with google auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,23 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <a href="/pagetwo">Naviagte to Page Two</a>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo<button>Click Me!</button>
-      <a href="/">Naviagte to Page One</a>
-    </div>
-  );
-};
 const App = () => {
   return (
-    <div>
+    <div className="ui contrainer">
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" exact component={PageTwo} />
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
